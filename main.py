abed4412627ce2e169c854b8c7991b3b11e033aa
MuzaffarShaikh0201 <muzaffarshaikh0201@gmail.com>
2023-04-19 05:20:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+from home import Home
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Home, '/')
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
