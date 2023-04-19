96130e9b594a90dc74c82786b97835db7d34c2a6
MuzaffarShaikh0201 <muzaffarshaikh0201@gmail.com>
2023-04-19 11:38:20 +0530

    Text commit 2

--- github_api.py
+++ github_api.py
@@ -6,6 +6,6 @@ class GithubApi(Resource):
     def post(self):
         if request.headers["Content-Type"] == "application/json":
             commits = request.json['commits'][0]
-            data.append(list(map(dict, commits)))
+            # data.append(list(map(dict, commits)))
             print(commits)
-            print(data)
+            # print(data)
